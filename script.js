c2307e9a233bbf9a31e8214ea24c4b3d730f1dca
atomic_coding <101187384+Sri-dinesh@users.noreply.github.com>
2024-07-02 16:53:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// <Script for Toggle Menu>
+var MenuItems = document.getElementById("MenuItems");
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+  if (MenuItems.style.maxHeight == "0px") {
+    MenuItems.style.maxHeight = "200px";
+  } else {
+    MenuItems.style.maxHeight = "0px";
+  }
+}
+
+// Script for Toggle Form
+
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+function register() {
+  RegForm.style.transform = "translateX(0px)";
+  LoginForm.style.transform = "translateX(0px)";
+  Indicator.style.transform = "translateX(100px)";
+}
+
+function login() {
+  RegForm.style.transform = "translateX(300px)";
+  LoginForm.style.transform = "translateX(300px)";
+  Indicator.style.transform = "translateX(0px)";
+}
+
+// Script for Product-img
+var ProductImg = document.getElementById("productImg");
+var SmallImg = document.getElementsByClassName("small-img");
+SmallImg[0].onclick = function () {
+  ProductImg.src = SmallImg[0].src;
+};
+SmallImg[1].onclick = function () {
+  ProductImg.src = SmallImg[1].src;
+};
+SmallImg[2].onclick = function () {
+  ProductImg.src = SmallImg[2].src;
+};
+SmallImg[3].onclick = function () {
+  ProductImg.src = SmallImg[3].src;
+};
